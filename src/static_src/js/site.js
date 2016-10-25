8c8d0553274dac3c51c859d6dfef3493295bea5c
Marfa <mary.dergay@gmail.com>
2016-10-25 16:27:53 +0300

    Make one jquery function for two sliders

--- src/static_src/js/site.js
+++ src/static_src/js/site.js
@@ -1,11 +1,31 @@
-var currentIndex = 0,
-    items = $(".spread"),
-    itemAmt = items.length;
+function initSlider(items, nextButton, previousButton, activationClass) {
+    var currentIndex = 0,
+        itemAmt = items.length;
 
-function cycleItems() {
-    var item = $(".spread").eq(currentIndex);
-    items.removeClass("spread_active");
-    item.addClass("spread_active");
+    function cycleItems() {
+        var item = items.eq(currentIndex);
+
+        items.removeClass(activationClass);
+        item.addClass(activationClass);
+    }
+
+    nextButton.click(function() {
+        currentIndex += 1;
+        if (currentIndex > itemAmt - 1) {
+            currentIndex = 0;
+        }
+        cycleItems();
+    });
+
+    previousButton.click(function() {
+
+        currentIndex -= 1;
+        if (currentIndex < 0) {
+            currentIndex = itemAmt - 1;
+        }
+        cycleItems();
+    });
+
 }
 
 $(document).ready(function() {
@@ -14,22 +34,8 @@ $(document).ready(function() {
         $(this).toggleClass("search-form_active");
     });
 
-    $(".listing-right").click(function() {
-        currentIndex += 1;
-        if (currentIndex > itemAmt - 1) {
-            currentIndex = 0;
-        }
-        cycleItems();
-    });
-
-    $(".listing-left").click(function() {
-
-        currentIndex -= 1;
-        if (currentIndex < 0) {
-            currentIndex = itemAmt - 1;
-        }
-        cycleItems();
-    });
+    initSlider($(".spread"), $(".preview__listing-right"), $(".preview__listing-left"), "spread_active");
+    initSlider($(".portfolio__blocks"), $(".portfolio__listing-right"), $(".portfolio__listing-left"), "portfolio_active");
 
     $("#kyiv, #kharkiv, #dnipro, .listing-left, .listing-right").click(function(event){
         event.preventDefault();
